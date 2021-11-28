430f2737768dbab9690abcda12fbabb7a00d2758
jtumidanski <jtumidanski@gmail.com>
2021-11-28 14:58:45 -0500

    Add "service" to all log messages. Log output with ecslogrus.Formatter.

--- atlas.com/mdc/main.go
+++ atlas.com/mdc/main.go
@@ -15,7 +15,7 @@ import (
 const serviceName = "atlas-mdc"
 
 func main() {
-	l := logger.CreateLogger()
+	l := logger.CreateLogger(serviceName)
 	l.Infoln("Starting main service.")
 
 	wg := &sync.WaitGroup{}
